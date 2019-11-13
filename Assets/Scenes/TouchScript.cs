df880c7454fe2b04d37165c17e5e66cf3a908524
Axeident <hmanlick@gmail.com>
2019-11-13 15:33:06 -0500

    touch added
    works
    position is a problem, why is the player so high in the air???

---
+++ Assets/Scenes/TouchScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchScript : MonoBehaviour
+{
+    // List of gameobjects for possible outcomes
+    public List<GameObject> LeadsTo;
+    // List of trigger items
+    private List<Transform> triggers;
+    
+
+    public void selectionMade(int optionChosen)
+    {
+        
+        //Make next scene activation based on choice made/trigger looked at. Should only be 2 options, if more are added,
+        //will need to add more trigger tags as well. Fairly simple to do.
+        LeadsTo[optionChosen].SetActive(true);
+        
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.GetComponent<GrabbableRespawn>())
+            {
+                child.gameObject.GetComponent<GrabbableRespawn>().respawning = false;
+            }
+        }
+
+    }
+    
+}
---
+++ Assets/Scenes/TouchSelectScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSelectScript : MonoBehaviour
+{
+    // used to make sure that triggers do not continue after scene has been completed
+    private bool runningScript;
+
+    public GameObject parent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        runningScript = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (runningScript)
+        {
+            if (other.CompareTag("TriggerOne"))
+            {
+
+                //GetComponentInParent<TouchScript>().selectionMade(0);
+                parent.GetComponent<TouchScript>().selectionMade(0);
+
+
+                this.gameObject.SetActive(false);
+                //Deactivate the script, so it doesn't cause strange issues later on in the engine.
+                runningScript = false;
+            }
+            if (other.CompareTag("TriggerTwo"))
+            {
+                
+                //GetComponentInParent<TouchScript>().selectionMade(1);
+                parent.GetComponent<TouchScript>().selectionMade(1);
+
+
+                this.gameObject.SetActive(false);
+                //Deactivate the script, so it doesn't cause strange issues later on in the engine.
+                runningScript = false;
+            }
+        }
+    }
+    
+}
