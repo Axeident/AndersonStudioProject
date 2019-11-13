a2281158e4d8714b102008a555777c82c616df83
axeident <hmanlick@gmail.com>
2019-11-13 11:34:56 -0500

    Added some grabbables
    I have no idea how things are being triggered to transition here. Need to figure that out

---
+++ Assets/Scenes/GrabbableRespawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Respawns and object at it's starting position if it gets too far away from the player
+/// </summary>
+public class GrabbableRespawn : MonoBehaviour
+{
+    private Vector3 spawnpoint;
+    /// <summary>
+    /// Maximum distance the object can be from the player before it respawns
+    /// </summary>
+    public float maxDistance = 5f;
+    void Start()
+    {
+        spawnpoint = this.transform.position;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, this.transform.position) > maxDistance)
+        {
+            this.transform.position = spawnpoint;
+        }
+    }
+}
