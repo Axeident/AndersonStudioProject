8bd6e20209b8cf1b8ab9c051f08ba0912fc9bbda
Neohanja Thalb <43677824+Neohanja@users.noreply.github.com>
2019-11-20 13:55:48 -0500

    I broke everything.

--- Assets/Scenes/TouchSelectScript.cs
+++ Assets/Scenes/TouchSelectScript.cs
@@ -6,6 +6,7 @@ public class TouchSelectScript : MonoBehaviour
 {
     // used to make sure that triggers do not continue after scene has been completed
     private bool runningScript;
+    private List<Transform> trigs;
 
     public GameObject parent;
 
@@ -13,6 +14,14 @@ public class TouchSelectScript : MonoBehaviour
     void Awake()
     {
         runningScript = true;
+        trigs = new List<Transform>();
+        foreach(Transform getTrig in transform)
+        {
+            if(getTrig.CompareTag("TriggerOne") || getTrig.CompareTag("TriggerTwo"))
+            {
+                trigs.Add(getTrig);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,8 +36,19 @@ public class TouchSelectScript : MonoBehaviour
 
 
                 this.gameObject.SetActive(false);
+
+                //disable touch movement on the objects in the scene
+                foreach(Transform trigOff in trigs)
+                {
+                    //trigOff.GetComponent<GrabbableRespawn>().enabled = false;
+                    //trigOff.GetComponent<OVRGrabbable>().enabled = false;
+                    //Destroy(trigOff.GetComponent<Rigidbody>());
+                    trigOff.gameObject.SetActive(false);
+                }
+
                 //Deactivate the script, so it doesn't cause strange issues later on in the engine.
                 runningScript = false;
+                
             }
             if (other.CompareTag("TriggerTwo"))
             {
@@ -38,6 +58,16 @@ public class TouchSelectScript : MonoBehaviour
 
 
                 this.gameObject.SetActive(false);
+
+                //disable touch movement on the objects in the scene
+                foreach (Transform trigOff in trigs)
+                {
+                    //trigOff.GetComponent<GrabbableRespawn>().enabled = false;
+                    //trigOff.GetComponent<OVRGrabbable>().enabled = false;
+                    //Destroy(trigOff.GetComponent<Rigidbody>());
+                    trigOff.gameObject.SetActive(false);
+                }
+
                 //Deactivate the script, so it doesn't cause strange issues later on in the engine.
                 runningScript = false;
             }
