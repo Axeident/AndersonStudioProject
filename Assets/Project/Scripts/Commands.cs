78c1cd2fbfcf4be8a142484143d0010ca420f69f
Neohanja Thalb <43677824+Neohanja@users.noreply.github.com>
2020-02-07 13:15:13 -0500

    started working on menu system.

---
+++ Assets/Project/Scripts/Commands.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Commands : MonoBehaviour
+{
+    public void StartSimulation()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+
+    public void QuitSimulation()
+    {
+        Application.Quit();
+    }
+}
